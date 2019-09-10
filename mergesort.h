162aeba5ac83d27c694ca6a21b6c00f00ffd0fca
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2019-09-10 20:42:14 +0530

    Update mergesort.h

--- mergesort.h
+++ mergesort.h
@@ -3,7 +3,7 @@
 
 
 
-struct val mergeSort(int a[], int l, int r);
+struct val mergeSort(int a[], int l, int r); // These are the functions which are used in Merge Sort Program
 
 void merge(int arr[], int l, int m, int r);
 
