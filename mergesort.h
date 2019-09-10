c90ae35dcd86e384ff94ea8fb714f55e51e1e672
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2019-09-10 13:09:18 +0530

    Add files via upload

---
+++ mergesort.h
@@ -0,0 +1,11 @@
+#ifndef MERGESORT_H_INCLUDED
+#define MERGESORT_H_INCLUDED
+
+struct val { double time; int swap; int comp; };
+
+struct val mergeSort(int a[], int l, int r);
+
+void merge(int arr[], int l, int m, int r);
+
+
+#endif // MERGESORT_H_INCLUDED
