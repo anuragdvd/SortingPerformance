77695dcb76665d77fe2a3c1b4ec7b022eee69a46
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2019-09-10 20:42:19 +0530

    Update quicksort.h

--- quicksort.h
+++ quicksort.h
@@ -1,7 +1,7 @@
 #ifndef QUICKSORT_H_INCLUDED
 #define QUICKSORT_H_INCLUDED
 
-struct val quick(int array[],int n);
+struct val quick(int array[],int n); // These are the functions which are used in Quick Sort Program
 void quicksort(int array[],int first,int last);
 
 #endif // QUICKSORT_H_INCLUDED
