c48d114ee3be7b077783ee2f2e091f9630c55423
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2019-09-10 20:42:06 +0530

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -2,7 +2,7 @@
 #define BUBBLE_H_INCLUDED
 
 
-struct val bubblesort (int a[],int n);
+struct val bubblesort (int a[],int n); //// These are the functions which are used in Bubble Sort Program
 
 
 #endif // BUBBLE_H_INCLUDED
