2960a1013086518927d0da4865cb01e351d6fc64
Vivekagent47 <vivek.agent47@gmail.com>
2019-09-17 15:33:25 +0530

    Bash script

---
+++ bash.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "Input type "
+echo
+echo "1 -> Array from keyboard"
+echo "2 -> Random Array"
+echo 
+read -p 'Select any type of input : ' val
+
+flag=0
+case $val in 
+    "1" )
+        cc main.c
+        ./a.out $val
+        flag=`expr $flag + 0` ;;
+    "2" )
+        cc main.c
+        ./a.out $val
+        flag=`expr $flag + 1` ;;
+    * )
+        echo "Wrong Input" ;;
+esac
+
+if [ $flag -eq 1 ]
+then
+    python3 graph.py
+fi
