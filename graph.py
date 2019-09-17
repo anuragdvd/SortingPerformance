c16ab50289521d9abdc08afa9c4b479a5e162c48
Vivekagent47 <vivek.agent47@gmail.com>
2019-09-17 15:10:01 +0530

    Updated

--- graph.py
+++ graph.py
@@ -21,28 +21,16 @@ comp=[]
 execution=[]
 
 #opening file to import swap data
-with open('swapout.txt','r') as csvfile:
+with open('out.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         elements.append(int(row[0]))
         swap.append(int(row[1]))
+        comp.append(int(row[2]))
+        execution.append(float(row[3]))
 no_elements = np.array(elements)
 swap_data = np.array(swap)
-
-
-#opening file to import comparsion data
-with open('compout.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        comp.append(int(row[1]))
 comp_data = np.array(swap)
-
-
-#opening file to import execution data
-with open('executionout.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        execution.append(float(row[1]))
 execution_data = np.array(execution)
 
 #dividing graph into subplots
@@ -54,16 +42,16 @@ ax3=fig.add_subplot(212)
 #plotting swaps
 ax1.set_xlabel("Number of elements")
 ax1.set_ylabel("Number of swaps")
-ax1.bar(no_elements,swap_data,color='red',width=1)
+ax1.plot(no_elements,swap_data)#,color='red',width=1)
 
 #plotting comparsion
 ax2.set_xlabel("Number of elements")
 ax2.set_ylabel("Number of comparsion")
-ax2.bar(no_elements,comp_data,color='red',width=1)
+ax2.plot(no_elements,comp_data)#,color='red',width=1)
 
 #plotting execution time
 ax3.set_xlabel("Number of elements")
 ax3.set_ylabel("Execution Time")
-ax3.bar(no_elements,execution_data,color='red',width=1)
+ax3.plot(no_elements,execution_data)#color='red',width=1)
 
 pl.show()
