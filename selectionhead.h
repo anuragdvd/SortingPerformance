65ffea8d1445d1567ca6bf953c3f2726a3ef9604
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2019-09-19 14:26:58 +0530

    Delete selectionhead.h

--- selectionhead.h
+++
@@ -1,6 +0,0 @@
-#ifndef SELECTIONHEAD_H_INCLUDED
-#define SELECTIONHEAD_H_INCLUDED
-
-struct val selectionsort(int a[],int n); // These are the functions which are used in Selection Sort Program
-
-#endif // SELECTIONHEAD_H_INCLUDED
