b79527498bca5714f88e18aa7628c8014f587ec1
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2019-09-19 14:24:09 +0530

    Delete counting.h

--- counting.h
+++
@@ -1,8 +0,0 @@
-#ifndef COUNTING_H_INCLUDED
-#define COUNTING_H_INCLUDED
-
-void countsort(int ar[],int,int); // These are the functions which are used in Counting Sort Program
-
-struct val countingsort(int ar[],int);
-
-#endif // COUNTING_H_INCLUDED
